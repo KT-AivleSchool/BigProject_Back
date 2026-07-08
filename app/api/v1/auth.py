6146237df5800d4835e2033ab1414ed9a0101aa9
chanjinjung <igt2430@gmail.com>
2026-07-08 10:22:57 +0900

    응답 스키마 수정, 주석 들여쓰기 수정, 미사용 Import 정리

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from pydantic import BaseModel, EmailStr
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 import asyncio
